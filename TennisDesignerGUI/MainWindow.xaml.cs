2b57b45d65231a141aca19940240370335c31491
Fabiola Espinoza <fabiolaeh9414@gmail.com>
2014-03-31 20:41:49 +0200

    prueba

--- TennisDesignerGUI/MainWindow.xaml.cs
+++ TennisDesignerGUI/MainWindow.xaml.cs
@@ -216,6 +216,9 @@ namespace TennisDesignerGUI
             Canvas.SetTop(segmentA, 132);
             canvasEdit.Children.Add(segmentA);
 
+            // SegmentC: line
+            segmentC = new Line();
+
         }
     }
 
