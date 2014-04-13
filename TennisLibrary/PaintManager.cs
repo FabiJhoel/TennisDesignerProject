a002c0e9391f3f33be2a33d70ba2f64096b9b546
JhoelMG <jhoelmg21@gmail.com>
2014-04-12 21:30:36 -0600

    Now it can draw lines from a downloaded designs, arcs not yet.

--- TennisLibrary/PaintManager.cs
+++ TennisLibrary/PaintManager.cs
@@ -17,11 +17,14 @@ namespace TennisLibrary
         public static void loadBasePoints(Design pDesign, Canvas pCanvas)
         {
             // Asign BasePoints to the new design
-            pDesign.addPoint(new BasePoint(156, 122, "pointA"));
-            pDesign.addPoint(new BasePoint(352, 122, "pointB"));
-            pDesign.addPoint(new BasePoint(457, 179, "pointC"));
-            pDesign.addPoint(new BasePoint(516, 269, "pointD"));
-            pDesign.addPoint(new BasePoint(156, 269, "pointE"));
+            if (pDesign.getBasePoints().Count == 0)
+            {   
+                pDesign.addPoint(new BasePoint(156, 122, "pointA"));
+                pDesign.addPoint(new BasePoint(352, 122, "pointB"));
+                pDesign.addPoint(new BasePoint(457, 179, "pointC"));
+                pDesign.addPoint(new BasePoint(516, 269, "pointD"));
+                pDesign.addPoint(new BasePoint(156, 269, "pointE"));
+            }
 
             // Draw each Basepoint on screen
             foreach (BasePoint point in pDesign.getBasePoints())
@@ -110,6 +113,80 @@ namespace TennisLibrary
             pDesign.getSegmentE().Y1 = 285;
             pDesign.getSegmentE().X2 = 165;
             pDesign.getSegmentE().Y2 = 285;
+            pDesign.getSegmentE().Stroke = System.Windows.Media.Brushes.Black;
+            pDesign.getSegmentE().StrokeThickness = 3;
+            pCanvas.Children.Add(pDesign.getSegmentE());
+        }
+
+        public static void loadTennisSilhouetteee(Design pDesign, Canvas pCanvas)
+        {
+            /*//Grid segmentAContainer;
+
+            // SegmentA: arc
+            PathFigure myPathFigure = new PathFigure();
+            myPathFigure.StartPoint = new Point(30, 40);
+            myPathFigure.Segments.Add(new ArcSegment(new Point(30, 190), new Size(50, 55),
+                                        0, true, SweepDirection.Counterclockwise, true));
+
+            PathGeometry myPathGeometry = new PathGeometry();
+            myPathGeometry.Figures.Add(myPathFigure);
+
+            pDesign.getSegmentA().Height = Double.NaN;
+            pDesign.getSegmentA().Width = Double.NaN;
+            pDesign.getSegmentA().Stretch = Stretch.Fill;
+            pDesign.getSegmentA().Stroke = Brushes.Black;
+            pDesign.getSegmentA().StrokeThickness = 3;
+            pDesign.getSegmentA().Data = myPathGeometry;
+
+            //pDesign.getSegmentAContainer().Background = System.Windows.Media.Brushes.LightBlue;
+            Canvas.SetLeft(pDesign.getSegmentAContainer(), 100);
+            Canvas.SetTop(pDesign.getSegmentAContainer(), 132);
+            pCanvas.Children.Add(pDesign.getSegmentAContainer());*/
+
+            // SegmentB: arc
+            PathFigure myPathFigureB = new PathFigure();
+            myPathFigureB.StartPoint = new Point(30, 40);
+            myPathFigureB.Segments.Add(new ArcSegment(new Point(227, 40), new Size(70, 30),
+                                       0, true, SweepDirection.Counterclockwise, true));
+
+            PathGeometry myPathGeometryB = new PathGeometry();
+            myPathGeometryB.Figures.Add(myPathFigureB);
+
+            pDesign.getSegmentB().Height = Double.NaN;
+            pDesign.getSegmentB().Width = Double.NaN;
+            pDesign.getSegmentB().Stretch = Stretch.Fill;
+            pDesign.getSegmentB().Stroke = Brushes.Black;
+            pDesign.getSegmentB().StrokeThickness = 3;
+            pDesign.getSegmentB().Data = myPathGeometryB;
+
+            //pDesign.getSegmentBContainer().Background = System.Windows.Media.Brushes.LightBlue;
+            Canvas.SetLeft(pDesign.getSegmentBContainer(), 165);
+            Canvas.SetTop(pDesign.getSegmentBContainer(), 132);
+            pCanvas.Children.Add(pDesign.getSegmentBContainer());
+
+            // SegmentC: line
+            pDesign.getSegmentC().X1 = pDesign.getBasePoints()[1].getAxisX() + 15;
+            pDesign.getSegmentC().Y1 = pDesign.getBasePoints()[1].getAxisY() + 15;
+            pDesign.getSegmentC().X2 = pDesign.getBasePoints()[2].getAxisX() + 15;
+            pDesign.getSegmentC().Y2 = pDesign.getBasePoints()[2].getAxisY() + 15;
+            pDesign.getSegmentC().Stroke = System.Windows.Media.Brushes.Black;
+            pDesign.getSegmentC().StrokeThickness = 3;
+            pCanvas.Children.Add(pDesign.getSegmentC());
+
+            // SegmentD: line
+            pDesign.getSegmentD().X1 = pDesign.getBasePoints()[2].getAxisX() + 15;
+            pDesign.getSegmentD().Y1 = pDesign.getBasePoints()[2].getAxisY() + 15;
+            pDesign.getSegmentD().X2 = pDesign.getBasePoints()[3].getAxisX() + 15;
+            pDesign.getSegmentD().Y2 = pDesign.getBasePoints()[3].getAxisY() + 15;
+            pDesign.getSegmentD().Stroke = System.Windows.Media.Brushes.Black;
+            pDesign.getSegmentD().StrokeThickness = 3;
+            pCanvas.Children.Add(pDesign.getSegmentD());
+
+            // SegmentE: line
+            pDesign.getSegmentE().X1 = pDesign.getBasePoints()[3].getAxisX()+15;
+            pDesign.getSegmentE().Y1 = pDesign.getBasePoints()[3].getAxisY()+15;
+            pDesign.getSegmentE().X2 = pDesign.getBasePoints()[4].getAxisX()+15;
+            pDesign.getSegmentE().Y2 = pDesign.getBasePoints()[4].getAxisY()+15;
             pDesign.getSegmentE().Stroke = System.Windows.Media.Brushes.Black;
             pDesign.getSegmentE().StrokeThickness = 3;
             pCanvas.Children.Add(pDesign.getSegmentE());
