fade4462f52494346dd9f8dfe94036f4e6333091
Fabiola Espinoza <fabiolaeh9414@gmail.com>
2014-04-25 17:04:36 -0600

    Arcade

--- TennisDesignerGUI/MainWindow.xaml.cs
+++ TennisDesignerGUI/MainWindow.xaml.cs
@@ -18,7 +18,6 @@ using DataAccess;
 using TennisBusiness;
 using TennisLibrary;
 
-
 namespace TennisDesignerGUI
 {
     public partial class MainWindow : Window
@@ -30,7 +29,7 @@ namespace TennisDesignerGUI
         {
             InitializeComponent();
             cmbxColor.ItemsSource = typeof(Colors).GetProperties();
-            DataManager.loadDesignList(ListBoxDesigns);
+            DataManager.loadDesignList(ListBoxDesigns);         
         }
         
         private void addNewDesignButton(object sender, RoutedEventArgs e)
